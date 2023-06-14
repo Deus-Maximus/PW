3fb81c92633e9c4df8e1878f7d535b1902c73a5f
Deus-Maximus <Silas.boventer@gmail.com>
2023-06-14 14:34:36 +0200

    same as before

--- validate-password.php
+++ validate-password.php
@@ -1,16 +1,16 @@
 <?php
-// Retrieve the entered password from the request body
-$requestBody = file_get_contents('php://input');
-$data = json_decode($requestBody, true);
-$enteredPassword = $data['password'];
+// Retrieve the entered password from the request
+$enteredPassword = $_POST['password'];
 
 // Define the correct password
 $correctPassword = '1234';
 
-// Validate the entered password
-$accessGranted = ($enteredPassword === $correctPassword);
-
-// Send the response as JSON
-header('Content-Type: application/json');
-echo json_encode(array('accessGranted' => $accessGranted));
+// Check if the entered password matches the correct password
+if ($enteredPassword === $correctPassword) {
+    // Password is correct, return a success response
+    echo 'success';
+} else {
+    // Password is incorrect, return an error response
+    echo 'error';
+}
 ?>
